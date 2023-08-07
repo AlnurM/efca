5732073fd610639ef3c6fd652812221f6f0cb082
AlnurM <alnur.mustafayev@gmail.com>
2023-08-07 12:20:38 +0600

    fix: Vacancy Details

--- src/pages/vacancy/[id]/index.jsx
+++ src/pages/vacancy/[id]/index.jsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'next-i18next'
+import React from 'react'
+import parse from 'html-react-parser'
 import { Container } from '@/shared/ui'
 import { api } from '@/shared/api'
 
@@ -10,6 +12,7 @@ const VacancyDetails = ({ data }) => {
     <>
       <Head>
         <title>{t('vacancy.head') || ''} | {data?.title || ''}</title>
+        <meta name="description" content={data?.text} />
       </Head>
       <section className="py-10">
         <Container shrink className="flex-col">
@@ -22,8 +25,40 @@ const VacancyDetails = ({ data }) => {
               {t('vacancy.active')}
             </div>
           </div>
+          <p className="mt-12 text-2xl font-medium">{data.text}</p>
         </Container>
       </section>
+      <section className="mt-6 py-10 bg-secondary">
+        <Container shrink className="flex-col">
+          {data.blocks.map(item => (
+            <ModifiedJSX key={item.id} html={item.html} />
+          ))}
+        </Container>
+      </section>
+    </>
+  )
+}
+
+const ModifiedJSX = ({ html }) => {
+  const parsedHTML = parse(html)
+  const applyStyles = (element) => {
+    if (React.isValidElement(element)) {
+      const elementType = element.type
+      if (elementType === 'ul') {
+        return React.cloneElement(element, { className: 'custom-ul' }, React.Children.map(element.props.children, applyStyles))
+      } else if (elementType === 'li') {
+        return React.cloneElement(element, { className: 'my-6 text-lg font-medium flex items-center before:mr-6 before:min-w-[20px] before:h-[20px] before:bg-primaryLight before:rounded-full' })
+      } else if (elementType === 'p') {
+        return React.cloneElement(element, { className: 'my-3 text-2xl font-semibold' })
+      }
+    }
+  
+    return element
+  }
+  const jsx = React.Children.map(parsedHTML, applyStyles)
+  return (
+    <>
+      {jsx}
     </>
   )
 }
